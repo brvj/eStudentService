20881626aa7f7f10cac55071d2bf393842e212b6
Tijana Rosic <rosictijana95@gmail.com>
2021-03-14 14:27:09 +0100

    Repository methods have implemented for TeacherTitle & Teacher entity

---
+++ studentService/src/main/java/com/ftn/tseo2021/sf1513282018/studentService/contract/repository/teacher/TeacherTitleRepository.java
@@ -0,0 +1,22 @@
+package com.ftn.tseo2021.sf1513282018.studentService.contract.repository.teacher;
+
+import com.ftn.tseo2021.sf1513282018.studentService.model.jpa.teacher.TeacherTitle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Set;
+
+public interface TeacherTitleRepository extends JpaRepository<TeacherTitle, Integer> {
+
+    Set<TeacherTitle> findByName(String name);
+
+    @Query(value = "Select * from teacher_title tt where tt.teacher_title_id in (select t.teacher_title_id from teacher t where t.teacher_id = ?1)", nativeQuery = true)
+    TeacherTitle findByTeacherId(int teacherId);
+
+    @Query(value = "Select * from teacher_title tt where tt.teacher_title_id in (select t.teacher_title_id from teacher t where t.teacher_id = ?1 and t.institution_id = ?2)", nativeQuery = true)
+    TeacherTitle findByTeacherIdAndInstitutionId(int teacherId, int institutionId);
+
+    @Query(value = "Select * from teacher_title tt where tt.teacher_title_id in " +
+            "(select t.teacher_title_id from teacher t join teacher_teaching_course ttc on t.teacher_id = ttc.teacher_id = ?1 and ttc.course_id = ?2)", nativeQuery = true)
+    TeacherTitle findByTeacherIdAndCourseId(int teacherId, int courseId);
+}

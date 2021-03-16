67b5a76367fa14b8551b8ad286e490c1625c96b2
Nikola Zoric <nikola.se.zoric@gmail.com>
2021-03-16 09:47:20 +0100

    Created query methods in UserRepository

--- studentService/src/main/java/com/ftn/tseo2021/sf1513282018/studentService/contract/repository/user/UserRepository.java
+++ studentService/src/main/java/com/ftn/tseo2021/sf1513282018/studentService/contract/repository/user/UserRepository.java
@@ -1,9 +1,28 @@
 package com.ftn.tseo2021.sf1513282018.studentService.contract.repository.user;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.ftn.tseo2021.sf1513282018.studentService.model.common.UserType;
 import com.ftn.tseo2021.sf1513282018.studentService.model.jpa.user.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+	
+	Optional<User> findByUsername(String username);
+	
+	@Query("SELECT u from User u WHERE "
+			+ "u.institution.id = :institutionId AND "
+			+ "(:username is null OR u.username LIKE CONCAT('%', :username, '%')) AND "
+			+ "(:firstName is null OR lower(u.firstName) LIKE lower(CONCAT('%', :firstName, '%'))) AND "
+			+ "(:lastName is null OR lower(u.lastName) LIKE lower(CONCAT('%', :lastName, '%'))) AND "
+			+ "(:userType is null OR u.userType = userType")
+	Page<User> filterUsers(@Param("institutionId") int institutionId, @Param("username")String username, 
+							@Param("firstName") String firstName, @Param("lastName") String lastName, 
+							@Param("userType") UserType userType, Pageable pageable);
+	
 }

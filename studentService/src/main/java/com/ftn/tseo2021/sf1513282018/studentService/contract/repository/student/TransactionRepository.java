d4ca6c7b0b04f5bd0eb9015065ca578ca188d2de
Stefan Kockar <kockar.stefan@gmail.com>
2021-03-18 14:45:58 +0100

    Added filter and optional methods

--- studentService/src/main/java/com/ftn/tseo2021/sf1513282018/studentService/contract/repository/student/TransactionRepository.java
+++ studentService/src/main/java/com/ftn/tseo2021/sf1513282018/studentService/contract/repository/student/TransactionRepository.java
@@ -1,8 +1,16 @@
 package com.ftn.tseo2021.sf1513282018.studentService.contract.repository.student;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.ftn.tseo2021.sf1513282018.studentService.model.common.TransactionType;
+import com.ftn.tseo2021.sf1513282018.studentService.model.jpa.student.Student;
 import com.ftn.tseo2021.sf1513282018.studentService.model.jpa.student.Transaction;
 
 @Repository
@@ -10,4 +18,14 @@ public interface TransactionRepository extends JpaRepository<Transaction, Intege
 
     //filter(int financialCardId, TransactionType type, LocalDate startTransactionDate, 
 	//LocalDate endTransactionDate, String description, Pageable pageable);
+	
+	@Query("SELECT f from FinancialCard f WHERE" +
+			"s.financialCard.id = :financialCardId AND" +
+			"(:userType is null OR u.userType = :userType) AND" + 
+			"(:startTransactionDate is null OR f.startTransactionDate isAfter :startTransactionDate) AND" + 
+			"(:endTransactionDate is null OR f.endTransactionDate isBefore :endTransactionDate) AND" +
+			"(:description is null OR lower(f.description) LIKE lower(CONCAT('%', :description,'%'))) AND")
+	Page<Transaction> filterTransaction(@Param("financialCardId") int financialCardId, @Param("type") TransactionType type,
+            @Param("startTransactionDate") LocalDate startTransactionDate,@Param("endTransactionDate") LocalDate endTransactionDate,
+            @Param("description") String description, Pageable pageable);
 }

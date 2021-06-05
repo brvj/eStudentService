2cf3608009a4ee2b91ae1b7a93ef0d4b8632249e
Boris Janković <jankovicb0230@gmail.com>
2021-06-05 16:08:37 +0200

    Added
    CourseFilterOptions class

---
+++ studentService/src/main/java/com/ftn/tseo2021/sf1513282018/studentService/contract/dto/course/course/CourseFilterOptions.java
@@ -0,0 +1,14 @@
+package com.ftn.tseo2021.sf1513282018.studentService.contract.dto.course.course;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CourseFilterOptions {
+    private String name;
+}
